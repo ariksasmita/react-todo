4059da7a51e66e9a2e0d3127ccc101efb3cc7f3e
I Putu Arik Sasmita <ariksasmita@gmail.com>
2018-07-12 05:21:21 +0800

    Wire up AddTodo function

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,4 +1,5 @@
 var React = require('react')
+var uuid = require('node-uuid')
 
 var TodoList = require('TodoList')
 var AddTodo = require('AddTodo')
@@ -11,26 +12,34 @@ var TodoApp = React.createClass({
       searchText: '',
       todos: [
         {
-          id: 1,
+          id: uuid(),
           text: 'Walk the dog',
         },
         {
-          id: 2,
+          id: uuid(),
           text: 'Clean the yard',
         },
         {
-          id: 3,
+          id: uuid(),
           text: 'Make the bed',
         },
         {
-          id: 4,
+          id: uuid(),
           text: 'Feed yourself',
         },
       ],
     }
   },
   handleAddTodo: function(text) {
-    console.log('New Todo: ', text)
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: uuid(),
+          text,
+        }
+      ]
+    })
   },
   handleSearch: function( showCompleted, searchText ) {
     this.setState({
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -10,5 +10,15 @@ describe('TodoApp', () => {
   it("should exist", () => {
     expect(TodoApp).toExist()
   })
+
+  it('should add Todo onHandleAddTodo', () => {
+    var todoText = 'Test'
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp />)
+
+    todoApp.setState({ todos: [] })
+    todoApp.handleAddTodo(todoText)
+
+    expect(todoApp.state.todos[0].text).toBe(todoText)
+  })
 })
 
